921980e66c78bbe3da75952568dfcd08ed716d4d
buttercak3 <s.hubauer.sh@gmx.de>
2023-06-23 23:07:05 +0200

    Changes to be committed: 	new file:   my_app/app/__pycache__/database.cpython-310.pyc 	new file:   my_app/app/__pycache__/main.cpython-310.pyc 	new file:   my_app/app/__pycache__/model.cpython-310.pyc 	modified:   my_app/app/database.py

--- my_app/app/database.py
+++ my_app/app/database.py
@@ -3,7 +3,8 @@ from model import TokenModel
 import motor.motor_asyncio
 
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+#client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://root:example@mongo:27017/')
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
 database = client.FastAPI
 collection = database.Token
 
